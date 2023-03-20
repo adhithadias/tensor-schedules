6151085c6563cedbaad3f66b2b83ffdcdb9d71db
Logan Anderson <anderslt.purdue.edu>
2023-03-20 15:07:11 -0400

    temporary progress in complexity computation

--- src/complexity.py
+++ src/complexity.py
@@ -1,29 +1,35 @@
 from src.autosched import sched_enum
 from src.config import Config
 
+ignore_unfused = True
+
 # retrieve complexity of a given tensor schedule 
-def retrieve_complexity(schedule=[]) -> str:
+def retrieve_complexity(schedule=Config) -> str:
   complexity = ""
-  if len(schedule) > 0 and type(schedule[-1]) == list:
-    producer = retrieve_complexity(schedule[-2])
-    consumer = retrieve_complexity(schedule[-1])
+  if (len(schedule.input_idx_order) > 0 and type(schedule.input_idx_order[-1]) == list) or not schedule.fused:
+    producer = retrieve_complexity(schedule.input_idx_order[-2])
+    consumer = retrieve_complexity(schedule.input_idx_order[-1])
     if len(producer) > 0 and len(consumer) > 0:
-      complexity += "*".join(schedule[0:-2]) + "*"
+      complexity += "*".join(schedule.input_idx_order[0:-2]) + "*"
       complexity += "(" + producer + "+"
       return complexity + consumer + ")"
     elif len(producer) > 0:
-      return "*".join(schedule[0:-2]) + "*(" + producer + ")"
+      return "*".join(schedule.input_idx_order[0:-2]) + "*(" + producer + ")"
     elif len(consumer) > 0:
-      return "*".join(schedule[0:-2]) + "*(" + consumer + ")"
+      return "*".join(schedule.input_idx_order[0:-2]) + "*(" + consumer + ")"
     else:
-      return "*".join(schedule[0:-2])
+      return "*".join(schedule.input_idx_order[0:-2])
   else:
-    return "*".join(schedule)
+    return "*".join(schedule.input_idx_order)
 
 def retrieve_all_complexities(schedules = [Config]):
   complexities = []
   for schedule in schedules: 
-    complexities.append(retrieve_complexity(schedule.input_idx_order))
+    if schedule.fused:
+      complexities.append(retrieve_complexity(schedule))
+    # else:
+    #   complexities.append(retrieve_complexity(schedule))
+      
   return complexities
 
 accesses = {
