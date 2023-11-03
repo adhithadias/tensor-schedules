715dd11e1e75bb0863153152aaaba5c260ff5a7b
anderslt9 <anderslt@purdue.edu>
2023-11-03 10:17:34 -0400

    Fixed code generation

--- src/gen_taco_rep.py
+++ src/gen_taco_rep.py
@@ -1,5 +1,5 @@
 from src.config import Config
-from src.autosched import sched_enum
+from src.autosched import sched_enum, get_schedules_unfused
 import sys
 from copy import deepcopy
 from random import randint
@@ -19,24 +19,30 @@ class Gen_Test_Code:
         self.reorders = []
         self.extra_paths = []
         self.extra_reorders = []
+        self.no_fusion = False
+        
+        if config.prod == None or config.fused == 0:
+            self.no_fusion = True
         
         # retrieves all paths for fusion
         self.get_paths([], config)
         
-        self.get_extra_paths()
+        if not self.no_fusion: self.get_extra_paths()
         # print(self.paths, file=sys.stdout)
         
+        # sets all of the reorders for loop fusion
         for path in self.paths:
             # get all indices
             given_config = self.retrieve_path(path, config)
+            if given_config.fused == 0: continue
             # self.indices = set()
             # self.get_indices(given_config.input_idx_order)
             # reordering = self.get_reordering(given_config.input_idx_order)
-            
             reordering = given_config.original_idx_perm
             # print(reordering)
             
             if len(self.reorders) == 0:
+                if reordering == None: continue
                 self.reorders.append(reordering)
             else:
                 old_ordering = None
@@ -61,6 +67,7 @@ class Gen_Test_Code:
                     self.reorders.append(new_reorderings)
                 else: self.reorders.append([])                    
         
+        # get paths for leaf node schedules
         for extra_path in self.extra_paths:
             given_config = self.retrieve_path(extra_path, config)
             reordering = given_config.original_idx_perm
@@ -114,6 +121,7 @@ class Gen_Test_Code:
         
         for i, path in enumerate(self.paths + self.extra_paths):
             # add paths
+            if len(self.reorders) == 0 and self.no_fusion: continue
             if(len(path) > 0):  
                 self.add_vector(name=("path" + str(i)), init=("{" + ", ".join([str(el) for el in path]) + "}"))
             else:
@@ -122,6 +130,7 @@ class Gen_Test_Code:
         # print scheduling commands
         if (len(self.reorders) > 0 or len(self.extra_reorders)):
             self.print_data("stmt = stmt")
+            
         
         for i, reorder in enumerate(self.reorders):
             self.add_reorder(reorder, i)
@@ -131,6 +140,8 @@ class Gen_Test_Code:
                 self.add_loopfuse(len(config_to_split.prod.expr), config_to_split.prod_on_left, i)
             elif config_to_split.cons and not config_to_split.prod_on_left:
                 self.add_loopfuse(len(config_to_split.cons.expr) - 1, config_to_split.prod_on_left, i)
+            elif self.no_fusion:
+                continue
             else: 
                 assert SyntaxError
                 # parent_config = self.retrieve_path(self.paths[i][:-1], config)
@@ -142,7 +153,7 @@ class Gen_Test_Code:
         for i, extra_reorder in enumerate(self.extra_reorders):
             self.add_reorder(extra_reorder, i + len(self.reorders))
         
-        self.print_data(";", 2)
+        if len(self.reorders) != 0 or not self.no_fusion: self.print_data(";", 2)
         self.add_end()
     
     def print_data(self, data:str, num_tabs=1) -> None:
@@ -175,21 +186,21 @@ class Gen_Test_Code:
                 next_config = config.cons
             return self.retrieve_path(ordering[1:], next_config)
     
-    def get_paths(self, path:tuple, config:Config, is_root=True) -> None:
+    def get_paths(self, path:tuple, config:Config) -> None:
         """This retrieves all possible paths of fusion
 
         Args:
             path (tuple): path so far of fusion
             config (Config): Config class
         """
-        if type(config.input_idx_order[-1]) != tuple:
-            if is_root == True: self.paths.append(path)
+        if type(config.input_idx_order[-1]) != tuple and config.prod == None:
+            if self.no_fusion == True: self.paths.append(path)
             return
         else:
             self.paths.append(path)
             
             if len(config.input_idx_order) > 1 and type(config.input_idx_order[-2]) == tuple and len(config.input_idx_order[-2]) > 0 and type(config.input_idx_order[-2][-1]) == tuple:
-                self.get_paths(path + [0], config.prod, False)
+                self.get_paths(path + [0], config.prod)
             if len(config.input_idx_order) > 1 and type(config.input_idx_order[-1]) == tuple and len(config.input_idx_order[-1]) > 0 and type(config.input_idx_order[-1][-1]) == tuple:
                 self.get_paths(path + [1], config.cons, False)
             
@@ -504,20 +515,32 @@ if __name__ == "__main__":
         # 'C': [],
         # 'D': [],
     }
-    sched_enum('X', ['A','B','C','D'], accesses['X'], accesses, tensor_idx_order_constraints, schedules)
+    # sched_enum('X', ['A','B','C','D'], accesses['X'], accesses, tensor_idx_order_constraints, schedules)
+    cache = {}
+    schedules = list(get_schedules_unfused('X', accesses['X'], ('A','B','C','D'), accesses, ('i', 'j', 'k', 'l', 'm'), tensor_idx_order_constraints, 1, cache))
     print("\n")
     counter_printing = 15
     counter = 1
     test_num = 1
     
+    test_sched = Config('X', ('A', 'B', 'C', 'D'), ('i', 'j', 'k', 'l', 'm'), ('i', 'j', 'l', 'k', 'm'), 2)
+    test_sched.original_idx_perm = ('i', 'j', 'l', 'k', 'm')
+    # Write_Test_Code(test_sched, "loopfuse", "/home/shay/a/anderslt/tensor-schedules/src/tests-workspaces.cpp")
+    
+    # Write_Test_Code(schedules[0], "loopfuse", "/home/shay/a/anderslt/tensor-schedules/src/tests-workspaces.cpp")
+    # Write_Test_Code(schedules[0], "loopfuse", "/home/shay/a/anderslt/tensor-schedules/src/tests-workspaces.cpp")
+    # exit()
     for schedule in schedules:
         # if count_fusions(schedule) == 2 and schedule.fused:
+            if type(schedule.input_idx_order[-1]) != tuple:
+                Write_Test_Code(schedule, "loopfuse", "/home/shay/a/anderslt/tensor-schedules/src/tests-workspaces.cpp")
+                break
+              
             counter = (counter % counter_printing) + 1
             # if counter == counter_printing:
                 # Gen_Test_Code(schedule, "Test " + str(test_num), sys.stdout)
-            Write_Test_Code(schedule, "loopfuse", "tests-workspaces.cpp")
             test_num += 1
-            break
+            # break
             # break
           
 
